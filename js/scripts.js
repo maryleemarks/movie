592b7f02faa1f8650194be719729e04f9da67633
Mary Marks <maryleemarks@gmail.com>
2021-07-13 20:51:31 -0700

    add all the things

--- js/scripts.js
+++ js/scripts.js
@@ -14,9 +14,13 @@ function CreateMovie (titleI, timesI, basePriceI) {
 
 // Business Logic
 let bobTheBuilder = new CreateMovie("Bob Strikes Back", ["1:30","3:30","5:30","7:30","11:00"],12);
-let userInput = new UserInput("Bob Strikes Back", ["1:30","3:30","5:30","7:30","11:00"],12)
-
-console.log(bobTheBuilder.title === userInput.movieName);
-
+let userInput = new UserInput("Bob Strikes Back", ["1:30"],12);
+let movieArray = ["Bob Strikes Back", "Movie2", "etc..."]
+//console.log(bobTheBuilder.title === userInput.movieName);
+function ticketPriceDeterminer() {
+  if(userInput.title === movieArray[0]) {
+    
+  }
+}
 
 // User Interface Logic
